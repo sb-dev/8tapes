2c638bb66dda7b9da17028177ea65dad5005fbb4
Samir Benzenine <samir.benzenine@gmail.com>
2020-02-03 08:16:46 +0000

    Add desktop player

---
+++ src/components/player.js
@@ -0,0 +1,101 @@
+import './player.css'
+
+import React, { useEffect, useState } from "react";
+
+import { Icon } from 'antd';
+import YouTube from 'react-youtube';
+
+/**
+ * Youtube player
+ * 
+ * https://developers.google.com/youtube/iframe_api_reference#top_of_page
+ * 
+ * @param {*} props 
+ */
+export default function Player(props) {
+    const [player, setPlayer] = useState(null);
+    const [state, setState] = useState(null);
+
+    function togglePlay() {
+        if (state === 'playing') {
+            player.pauseVideo()
+        } else if (state === 'paused' || state === 'ended') {
+            player.playVideo()
+        }
+    }
+    
+    function _onReady(event) {
+        console.log(event.target)
+        setPlayer(event.target)
+    }
+
+    function _onStateChange({data}) {
+        switch(data) {
+            case 0:
+                setState('ended')
+                break
+            case 1:
+                setState('playing')
+                break
+            case 2:
+                setState('paused')
+                break
+            case 3:
+                setState('buffering')
+                break
+            case 5:
+                setState('video-cued')
+                break
+            default:
+                setState('unstarted')
+          }
+    }
+    
+    function renderSelectedVideo() {
+        const opts = {
+            width: '90px',
+            height: '60px',
+            playerVars: {
+            autoplay: 1,
+            controls: 0
+            }
+        };
+        
+        return (
+            <>
+                <YouTube
+                    className="image-wrapper"
+                    videoId={props.selectedItem.id}
+                    opts={opts}
+                    onReady={_onReady} 
+                    onStateChange={_onStateChange} />
+                <div className="now-playing-wrapper">
+                    <div className="now-playing-content">
+                    <div className="title">{props.selectedItem.title}</div>
+                    <div className="details">{props.selectedItem.channelTitle}</div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+      
+    return (
+        <div className={`player ${props.selectedItem ? 'active' : ''}`}>
+            <div className="left">
+                {props.selectedItem ? renderSelectedVideo() : ''}
+            </div>
+            <div className="center">
+                <Icon className="button" type="step-backward" />
+                <Icon 
+                    className="play-button"
+                    onClick={togglePlay} 
+                    type={state === 'playing' ? "pause-circle" : "play-circle"} 
+                    theme="filled" />
+                <Icon className="button" type="step-forward" />
+            </div>
+            <div className="right">
+                <Icon className="button" type="sound" theme="filled" />
+            </div>
+        </div>
+    )
+}
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from "react";
 import { Card } from 'antd';
 import Layout from '../components/layout';
 import LazyLoad from 'react-lazyload';
+import Player from '../components/player';
 import { isMobile } from 'react-device-detect';
 import loadLikedVideos from '../helpers/youtubeHelpers'
 import pizzaz from './pizzaz.gif';
@@ -41,33 +42,35 @@ export default function Browse(props) {
 
   function renderVideos() {
     return (
-      <Layout>
+      <>
+        <Layout>
             {videos.map((category, i) => { return (
               <div key={i}>
                 <h1>{category.label}</h1>
-        <Row>
+                <Row>
                 {category.videos.map((video, i) => {
                   return (<Col xl={3} lg={4} md={6} sm={8} xs={12} key={video.id}>
                     <LazyLoad offset={100} once>
-              <Card
-                className={'pointer video'}
+                      <Card
+                          className={'pointer video'}
                           onClick={()=> handleVideoClick(video) }
-                bodyStyle={{ display: 'none' }}
-                cover={
-                    <img
-                      alt={video.title}
-                      src={video.thumbnail ? video.thumbnail.url : ''}
-                    />
-                }
-              />
+                          bodyStyle={{ display: 'none' }}
+                          cover={
+                              <img
+                                alt={video.title}
+                                src={video.thumbnail ? video.thumbnail.url : ''}
+                              />
+                          }
+                        />
                     </LazyLoad>
                   </Col>)
                 })}
                 </Row>
               </div>
-          )})}
-        </Row>
-      </Layout>
+            )})}
+        </Layout>
+        {isMobile ? '' : <Player selectedItem={selected}/>}
+      </>
     );
   }
   
